8d6928fdb4b1110c10beb9b9759edbdc78cec91c
luongthanhdat0707 <luongthanhdat0707>
2020-05-04 13:53:00 +0700

    seekbar

--- app/src/main/java/com/example/progressbarseekbar25022020/MainActivity.java
+++ app/src/main/java/com/example/progressbarseekbar25022020/MainActivity.java
@@ -14,24 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressbar= findViewById(R.id.progressbar);
-
-        CountDownTimer countDownTimer = new CountDownTimer(1000,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                if(mProgressbar.getProgress() >=100){
-                    mProgressbar.setProgress(0);
-                }
-                mProgressbar.setProgress(mProgressbar.getProgress()+10);
-
-            }
-
-            @Override
-            public void onFinish() {
-                this.start();
-
-            }
-        };
-        countDownTimer.start();
+//        mProgressbar= findViewById(R.id.progressbar);
+//
+//        CountDownTimer countDownTimer = new CountDownTimer(1000,1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                if(mProgressbar.getProgress() >=100){
+//                    mProgressbar.setProgress(0);
+//                }
+//                mProgressbar.setProgress(mProgressbar.getProgress()+10);
+//
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//                this.start();
+//
+//            }
+//        };
+//        countDownTimer.start();
     }
 }
